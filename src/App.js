47bb28d86aa1cd6a233f953f09ceabe7e23a4c74
Aryan Dantanni <161295571+aryanDantani@users.noreply.github.com>
2026-02-23 22:30:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react'
+
 import Card from './components/Card'
+
 import Category from './components/Category'
+
 import Food from './components/Food'
+
 import Hero from './components/Hero'
+
 import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <>
+    
       <Navbar />
+    
       <Hero />
+    
       <Card />
+    
       <Food />
+    
       <Category />
+    
     </>
   )
 }
